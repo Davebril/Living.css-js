759f8d1f0421f7426192bafb725eb9ae62cb7655
Davebril <68717358+Davebril@users.noreply.github.com>
2020-10-21 13:37:08 -0300

    Add files via upload

---
+++ tpsillas.js
@@ -0,0 +1,30 @@
+window.onload = function () {
+	const chairs = document.querySelectorAll('.chair')
+	const addButton = document.querySelector('.button-add')
+	const removeButton = document.querySelector('.button-remove')
+
+	let showedChairs = 0
+
+	addButton.onclick = function () {
+		const chairToShow = chairs[showedChairs]
+
+		if (chairToShow === undefined) {
+			return
+		}
+
+		chairToShow.style.visibility = 'visible'
+
+		showedChairs += 1
+	}
+		removeButton.onclick = function () {
+			const chairToHide = chairs[showedChairs - 1]
+
+			if (chairToHide === undefined) {
+				return
+			}
+
+			chairToHide.style.visibility = 'hidden'
+
+			showedChairs -= 1
+		}
+}
